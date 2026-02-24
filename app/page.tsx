e836496bb79186e4722e48a3cae9fc972295c633
Muhammad Yousaf <yousufdevops09@gmail.com>
2026-02-24 14:54:44 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
